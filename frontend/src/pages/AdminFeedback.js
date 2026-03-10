b7c5264d98fb2eed1e170b4c00297674c891f7aa
krishu <krishupambhar12@gmail.com>
2026-03-10 16:06:23 +0530

    10/3/26

--- frontend/src/pages/AdminFeedback.js
+++ frontend/src/pages/AdminFeedback.js
@@ -20,7 +20,7 @@ const AdminFeedback = () => {
     deleting: null
   });
 
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token');         
 
   const fetchFeedbacks = useCallback(async () => {
     setLoading(true);
